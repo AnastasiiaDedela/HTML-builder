a54fecfcdad439debf0ee8294ed323e077dc99dd
Anastasiia Dedela <nastyadedela@gmail.com>
2023-05-07 19:50:27 +0200

    feat: complete fourth task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs/promises');
+const path = require('path');
 
+async function copyDirectory() {
+  const sourceDirectory = path.join(__dirname, 'files');
+  const destinationDirectory = path.join(__dirname, 'files-copy');
+
+  try {
+    await fs.mkdir(destinationDirectory, { recursive: true });
+    const content = await fs.readdir(sourceDirectory, { withFileTypes: true });
+    await Promise.all(
+      content.map(async (file) => {
+        const source = path.join(sourceDirectory, file.name);
+        const destination = path.join(destinationDirectory, file.name);
+        if (file.isDirectory()) {
+          await copyDir(source, destination);
+        } else {
+          await fs.copyFile(source, destination);
+        }
+      })
+    );
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+copyDirectory();
