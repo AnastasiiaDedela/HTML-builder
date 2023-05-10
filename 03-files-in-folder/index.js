cc18a7c440fd68fd1bf17f507c571035c6a42be5
Anastasiia Dedela <nastyadedela@gmail.com>
2023-05-10 19:18:52 +0200

    fix: fix 3 task

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -2,19 +2,20 @@ const fs = require('fs/promises');
 const path = require('path');
 
 async function readContent() {
-    try {
-        const content = await fs.readdir('./03-files-in-folder/secret-folder', { withFileTypes: true });
-        for (let file of content) {
-            if (file.isFile()) {
-                const stat = await fs.stat(`./03-files-in-folder/secret-folder/${file.name}`);
-                const size = stat.size / 1024;
-                const extName = path.extname(file.name).slice(1);
-                console.log(`${file.name} - ${extName} - ${(size.toFixed(3) + 'kb')}`);
-            }
-        }
-    } catch (error) {
-        console.error(error);
+  try {
+    const content = await fs.readdir('./03-files-in-folder/secret-folder', { withFileTypes: true });
+    for (let file of content) {
+      if (file.isFile()) {
+        const stat = await fs.stat(`./03-files-in-folder/secret-folder/${file.name}`);
+        const size = stat.size / 1024;
+        const extName = path.extname(file.name).slice(1);
+        const fileName = path.basename(file.name, `.${extName}`);
+        console.log(`${fileName} - ${extName} - ${(size.toFixed(3) + 'kb')}`);
+      }
     }
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 readContent();
